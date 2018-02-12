5f5b90db3c84edd4260c3684dd0f9255dc94a452
David Ferguson <deejai@live.com>
2018-02-12 18:02:11 -0500

    started implementing

--- ProgrammingAssignment1/roundrobin.c
+++ ProgrammingAssignment1/roundrobin.c
@@ -1,6 +1,17 @@
 #include "processors.h"
 
+typedef struct processQueue {
+    process p[MAX_PROCESSES];
+    struct processNode *next;
+} processNode;
+
 void runRoundRobin(FILE *ofp, process *processes, int numProcesses, int runfor, int quantum)
 {
+    sortByArrivalTime(processes, numProcesses);
 
+    int processIndex = 0;
+
+    for(int i=0; i<runfor; i++) {
+
+    }
 }
