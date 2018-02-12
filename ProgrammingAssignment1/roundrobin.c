ef7a2ac895f157578658c46b9a6bf27183e6dd43
David Ferguson <deejai@live.com>
2018-02-12 18:30:46 -0500

    mostly implemented round robin

--- ProgrammingAssignment1/roundrobin.c
+++ ProgrammingAssignment1/roundrobin.c
@@ -1,17 +1,56 @@
 #include "processors.h"
 
 typedef struct processQueue {
-    process p[MAX_PROCESSES];
-    struct processNode *next;
-} processNode;
+    process *p[MAX_PROCESSES];
+    int head, count;
+} processQueue;
+
+void enqueue(processQueue *q, process *p)
+{
+    q->p[(q->head + q->count) % MAX_PROCESSES] = p;
+    q->count++;
+}
+
+process *dequeue(processQueue *q)
+{
+    if(q->count == 0) {
+        return NULL;
+    }
+
+    process *p = q->p[q->head];
+    q->head = (q->head + 1) % MAX_PROCESSES;
+
+    return p;
+}
 
 void runRoundRobin(FILE *ofp, process *processes, int numProcesses, int runfor, int quantum)
 {
     sortByArrivalTime(processes, numProcesses);
 
+    processQueue q;
+    q.head = 0;
+    q.count = 0;
+
     int processIndex = 0;
 
-    for(int i=0; i<runfor; i++) {
+    process *currentProcess = NULL;
 
+    for(int timer=0; timer<runfor; timer++) {
+        while(processIndex < numProcesses && processes[processIndex].sleep == timer) {
+            enqueue(&q, &processes[processIndex]);
+            processIndex++;
+        }
+
+        if(timer % quantum == 0) {
+            enqueue(&q, currentProcess);
+            currentProcess = dequeue(&q);
+        }
+
+        if(currentProcess == NULL) {
+            fprintf(ofp, "Time %d: Idle\n", timer);
+        }
+        else {
+            fprintf(ofp, "Time %d: %s arrived\n", timer, currentProcess->name);
+        }
     }
 }
