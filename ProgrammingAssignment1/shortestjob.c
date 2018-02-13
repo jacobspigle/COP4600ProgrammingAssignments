19e4270970d4ebbaa1900a4295450c26f9e681fe
jacobspigle <jacobspigle@gmailcom>
2018-02-12 19:52:22 -0500

    untested shortestjob.c, waiting on gcc install

--- ProgrammingAssignment1/shortestjob.c
+++ ProgrammingAssignment1/shortestjob.c
@@ -2,5 +2,42 @@
 
 void runShortestJobFirst(FILE *ofp, process *processes, int numProcesses, int runfor)
 {
+    //initialize tick
+    int tick = 0;
+    process *shortestBurst = &processes[0];
 
+    //go through 'runfor' times
+    while (tick < runfor)
+    {
+        //find process with smallest burst
+        for (int i = 0; i < numProcesses; i++)
+        {
+            //notify if process has arrived
+            if (processes[i].sleep == tick)
+            {
+                printStatusLine(ofp, tick, &processes[i], shortestBurst->burst, "arrived");
+            }
+
+            //only if processes[i] still has > 0 burst AND is smaller than shortestBurst process
+            if ((processes[i].burst < shortestBurst->burst) && (processes[i].burst != 0))
+            {
+                shortestBurst = &processes[i];
+            }
+        }
+
+        //select process with shortest burst
+        printStatusLine(ofp, tick, shortestBurst, shortestBurst->burst, "selected");
+        
+        //decrement burst count
+        shortestBurst->burst--;
+
+        //process is finished if burst is now 0
+        if (shortestBurst->burst == 0)
+        {
+            printStatusLine(ofp, ++tick, shortestBurst, 0, "finished");
+        }
+        
+        //increment tick
+        tick++;
+    }
 }
