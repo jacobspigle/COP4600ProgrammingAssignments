b789dfa42ede7d20a11e7176c556ce357557507a
David Ferguson <deejai@live.com>
2018-02-16 00:26:08 -0500

    bubble sort ends early if no swaps were made in the last iteration

--- ProgrammingAssignment1/main.c
+++ ProgrammingAssignment1/main.c
@@ -203,6 +203,8 @@ void sortByArrivalTime(process *processes, int numProcesses)
 {
     // bubble sort
     for(int i=0; i<numProcesses; i++) {
+        
+        bool swapped = false;
         for(int j=0; j<numProcesses-i-1; j++) {
             int prevarrival = processes[j].arrival;
             int nextarrival = processes[j+1].arrival;
@@ -213,7 +215,13 @@ void sortByArrivalTime(process *processes, int numProcesses)
                 process temp = processes[j];
                 processes[j] = processes[j+1];
                 processes[j+1] = temp;
+
+                swapped = true;
             }
+        }
+
+        if(swapped == false) {
+            break;
         }
     }
 }
