0976a907a929ebee58dcf67eb983bf631a36d534
jacobspigle <jacobspigle@gmail.com>
2018-04-19 20:50:54 -0400

    ye

--- Assignment4/chardev-write/chardev-write.c
+++ Assignment4/chardev-write/chardev-write.c
@@ -120,7 +120,7 @@ static void replaceUCF(void)
 
             while((first_loop || index != head) && j < ucf_length)
             {
-                printk(KERN_INFO "while loop (%d)\n", j);
+                printk(KERN_INFO "while loop (j = %d) (index = %d)\n", j, index);
                 first_loop = 0;
                 queue[index] = ucf_string[j];
                 queueLen++;
